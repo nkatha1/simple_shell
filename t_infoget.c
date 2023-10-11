5ba8ec3c9b2fb846cfd31220273f480708f0ccd5
stringbitalx <stringbitalx@gmail.com>
2023-10-11 11:49:04 -0700

    updated files

--- t_infoget.c
+++ t_infoget.c
@@ -22,23 +22,23 @@ void set_info(info_t *info, char **av)
 	int i = 0;
 
 	info->fname = av[0];
-	if (!info->argv)
+	if (info->arg)
 	{
-
-		info->argv = malloc(sizeof(char *) * 2);
-		if (info->argv)
+		info->argv = strtow(info->arg, "\t");
+		if (!info->argv)
 		{
-			info->argv[0] = _strdup(info->arg);
-			info->argv[1] = NULL;
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
 		}
-	}
-}
-for (i = 0; info->argv && info ->argv[i]; i++)
-	;
-info->argc = i;
-
-replace_alias(info);
-replace_vars(info);
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;
+		replace_alias(info);
+		replace_vars(info);
 	}
 }
 
@@ -70,7 +70,4 @@ void free_info(info_t *info, int all)
 		_putchar(BUF_FLUSH);
 	}
 }
-
-
-
 
