da852c421ce8424417fcfad2670ccf12ce6b1187
nkathapatience@gmail.com <nkathapatience1@gmail.com>
2023-10-13 16:11:05 +0300

    s.p

---
+++ t_list.c
@@ -0,0 +1,162 @@
+#include "shell.h"
+
+/**
+ * add_node - To the start of a list it adds a node.
+ * @head: Pointers adress head node.
+ * @sr: The filed nodes string.
+ * @num: The used hist nodes index.
+ *
+ * Return: The lists size.
+ */
+list_t *add_node(list_t **head, const char *sr, int num)
+{
+	list_t *new_head;
+
+	if (!head)
+		return (NULL);
+	new_head = malloc(sizeof(list_t));
+	if (!new_head0
+			return (NULL);
+	_memset((void *)new_head, 0, sizeof(list_t));
+	new_head->num = num;
+	if (sr)
+	{
+		new_head->sr = _strdup(sr);
+		if (!new_head->sr)
+		{
+			free(new_head);
+			return (NULL);
+		}
+	}
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
+}
+
+/**
+ * add_node_end - To the end of a list it adds a node.
+ * @head: Pointer adresses node head.
+ * @sr: The field nodes string.
+ * @num : Used by history index node.
+ *
+ * Return: The lists size.
+ */
+list_t *add_node_end(list_t **head, const char *sr, int num)
+{
+	list_t *new_node, *node;
+
+	if (!head)
+		return (NULL);
+
+	node = *head;
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	_memset((void *)new_node, 0, sizeof(list_t));
+	new_node->num =num;
+	if (sr)
+	{
+		new_node->str = _strdup(sr);
+		if (!new_node->sr)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else 
+		*head = new_node;
+	return (new_node);
+}
+
+/**
+ * print_list_str - It only prints the element strings linked list_t list.
+ * @h: First head pointer.
+ *
+ * Return: The list size.
+ */
+size_t print_list_str(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(h->sr ? h->sr : "(nil)");
+		_puts("\n");\h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * delete_node_at_index - At a given index, it deletes a node.
+ * @head: The first nodes pointer adress.
+ * @index: The nodes delete index.
+ *
+ * Return: It returns on success 1, on failure returns 0.
+ */
+int delete_node_at_index(list_t **head, unsigned int index)
+{
+	list_t *node, *prev_node;
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (0);
+
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node->sr);
+		free(node);
+		return (1);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == index)
+		{
+			prev_node->next = node->next;
+			free(node->sr);
+			free(node);
+			return (1);
+		}
+		i++;
+		prev_node = node;
+		node = node->next;
+	}
+	return (0);
+}
+
+/**
+ * free_list - All the nodes list it frees them.
+ * @head_ptr : Head nodes pointr adress.
+ *
+ * Return : It returns void.
+ */
+void free_list(list_t **head_ptr)
+{
+	list-t *node, *next_node, *head;
+
+	if (1head_ptr || !*head_ptr)
+		return;
+	head = *head_ptr;
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->sr);
+		free(node);
+		node = next_node;
+	}
+	*head_ptr = NULL;
+}
+
+
+
+
