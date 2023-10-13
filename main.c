5a3a0710f2f408c4929eeb49f256ab225c79fde2
nkathapatience@gmail.com <nkathapatience1@gmail.com>
2023-10-13 15:29:45 +0300

    s.p

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * main - The point of entry.
+ * @ac: The argument count.
+ * @av: The vector argument.
+ *
+ * Return: On sucess it returns 0, on error it returns 1.
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], 0_rdonly);
+		if (fd ==  -1)
+		{
+			if (errno ==EACESS)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return(EXIT_SUCESS)
+}
