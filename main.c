c8e91217f027c3fb070cdae0a5339529682fbf61
nkathapatience@gmail.com <nkathapatience1@gmail.com>
2023-11-15 22:40:12 +0300

    files

--- main.c
+++ main.c
@@ -17,44 +17,31 @@ int main(int ac, char **av)
 		: "=r" (fd)
 		: "r" (fd));
 
-	if (ac == 2)
-	{
-<<<<<<< HEAD
-		fd = open(av[1], 0_RDONLY);
-=======
+	if (ac < 2) {
 		puts_fd(2, "Invalid argument count\n");
 		return (EXIT_FAILURE);
-	}
-	if (a == 2)
+				}
+
+	if (ac >=3)
 	{
-		fd = open(b[1], O_RDWR);
->>>>>>> 9d055b303cdf18de14f41d0cebf8a6623a82ec58
+		fd = open(av[1], O_RDWR);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit (126);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
-				_eputs(" : 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
+				puts_fd(2, "No entry\n");
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
 		info->readfd = fd;
 	}
-<<<<<<< HEAD
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-=======
 	create_envlist(info);
 	read_hist(info);
-	shel(info, b);
->>>>>>> 9d055b303cdf18de14f41d0cebf8a6623a82ec58
+	shel(info, av);
+
 	return (EXIT_SUCCESS);
 }
 
