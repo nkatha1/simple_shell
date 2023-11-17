accd3f0e01b7a05d67c463dfebcf040885116603
nkathapatience@gmail.com <nkathapatience1@gmail.com>
2023-11-17 23:49:08 +0300

    cp

--- history.c
+++ history.c
@@ -1,1 +1,47 @@
+#include "shell.h"
 
+/**
+ * history - The hist 
+ *
+ * @input: The character input
+ */
+int history(char *input)
+{
+	char *file = ".shell_history";
+	ssize_t ff;
+	ssize_t w;
+	int len = 0;
+
+
+	if (!file)
+		return (-1);
+
+	ff = open(file, 0_CREAT | 0_RDWR | O_APPEND, 00600);
+
+	if (ff < 0)
+		return (-1);
+	if (input)
+	{
+		while (input[len])
+			len++;
+		w = write(ff, input, len);
+		if (w < 0)
+			return (-1);
+	}
+	return (1);
+}
+
+/**
+ * free_env - It frees the environmnt variable
+ * @env: The environment variables
+ * Return: Void
+ */
+void free_env(char **env)
+{
+	int i;
+
+	for (i = 0; env[i]; i++)
+	{
+		free(env[i]);
+	}
+}
